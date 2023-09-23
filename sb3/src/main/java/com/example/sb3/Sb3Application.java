66e2f2b1e58f507525a191d4be3fe773357425d2
Augsorn Chanklad <uppban5@gmail.com>
2023-09-23 17:14:25 +0700

    Add project spring boot

---
+++ sb3/src/main/java/com/example/sb3/Sb3Application.java
@@ -0,0 +1,13 @@
+package com.example.sb3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb3Application.class, args);
+	}
+
+}
